2a73d2f23f7294387cf61e104386b9cffc2b54b9
RobertoRaggio <rraggioc@gmail.com>
2022-03-26 20:21:11 -0300

    editando el codigo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,51 +2,53 @@
   <div>
     <h1>Login Usuario</h1>
     <b-container class="bv-example-row">
-      <div>
+     <div>
         <input class="m-2" placeholder="E-mail" v-model="usuario.email" />
-        <input
-          class="m-2"
-          placeholder="Password"
-          v-model="usuario.password"
-          type="password"
-        />
-        <b-button class="m-3" variant="success" @click="iniciarSesion"
-          >Iniciar</b-button
-        >
-
+        <input class="m-2" placeholder="Password" v-model="usuario.password" type="password"/>
+        <b-button class="m-3" variant="success" @click="registrarUsuario">Iniciar</b-button>
         <b-button class="m-3" variant="danger">Limpiar Formulario</b-button>
-        <b-button class="m-3" variant="warning">Limpiar Validación</b-button>
+        
+        <p class="text-danger" v-if="error">{{error}}</p>
+        <b-button class="m-3" variant="warning" @click="error">Limpiar Validación</b-button>
+        
       </div>
     </b-container>
   </div>
 </template>
 
 <script>
-import { getAuth, signInWithEmailAndPassword } from "@firebase/auth";
 import { mapActions } from "vuex";
+//estado local email,usuario, contraseña
 export default {
-  data() {
+  methods: {
+    ...mapActions(["register_User"]),
+    async registrarUsuario() {
+      try {
+      const { usuario } = this;
+      //registro exitoso
+      await this.register_User(usuario);
+      this.usuario.email="";
+      this.usuario.password="";
+      this.error="";
+      }
+      //error de registro
+      catch (error) {
+        alert ("Error, No estas logeado");
+        this.error = error.code;
+      }
+    },
+  },
+    data() {
     return {
       usuario: {
         email: "",
         password: "",
       },
+      error: null,
     };
   },
-  methods: {
-    ...mapActions(["inciar_Sesion"]),
-    async iniciarSesion() {
-      const { email, password } = this.usuario;
-      const auth = getAuth();
-      await signInWithEmailAndPassword(auth, email, password).then(
-        (response) => {
-          console.log("Estas logeado!");
-          this.$router.push("/");
-        }
-      );
-    },
-  },
-};
+}
+
 </script>
 
 
